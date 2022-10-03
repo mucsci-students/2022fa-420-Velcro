c9225ac7e8af061fa807003d6055658e41a014df
gps137 <39245737+gps137@users.noreply.github.com>
2022-10-03 16:43:30 -0400

    Params & Fields & Methods
    Added client requested features after significant effort.

---
+++ src/main/velcro/Model/Attributes.java
@@ -0,0 +1,48 @@
+/**
+ * Filename: Attributes.java.
+ * 
+ * @author Jon Beare, Dylon McGrann, Greg Sinclair, Cole Stout, Benedikt Wagenlehner
+ * Course: CSCI 420 (Section 01) 
+ * Description: Attributes class.
+ * 
+ */
+
+
+
+package main.java.velcro.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Attributes {
+	
+	@SerializedName("name")
+	@Expose
+	String name;
+	String parentClass;
+	
+	// Attributes constructor.
+	Attributes(String name) {
+		this.name = name;
+	}
+	
+	// Returns Attributes name.
+	public String getName() {
+		return name;
+	}
+
+	
+	// Returns boolean of whether input Attributes object equals this Attributes object.
+	public boolean equals(Attributes input) {
+		return (input.name.equals(this.name));
+	}
+	
+	// Renames Attribute.
+	public boolean rename(String newName) {
+		if (newName.equals(""))
+			return false;
+		this.name = newName;
+		return true;
+	}
+	
+}
---
+++ src/main/velcro/Model/Fields.java
@@ -0,0 +1,57 @@
+/**
+ * Filename: Fields.java.
+ * 
+ * @author Jon Beare, Dylon McGrann, Greg Sinclair, Cole Stout, Benedikt Wagenlehner
+ * Course: CSCI 420 (Section 01) 
+ * Description: Fields class.
+ * 
+ */
+
+
+
+package main.java.velcro.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Fields {
+	
+	@SerializedName("name")
+	@Expose
+	String name;
+	String parentClass;
+	
+	@SerializedName("type")
+	@Expose
+	String type;
+	
+	// Fields constructor.
+	public Fields(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+	
+	// Returns Fields name.
+	public String getName() {
+		return name;
+	}
+
+	// Returns Fields type.
+	public String getType() {
+		return type;
+	}
+	
+	// Returns boolean of whether input Fields object equals this Fields object.
+	public boolean equals(Fields input) {
+		return (input.name.equals(this.name) && input.type.equals(this.type));
+	}
+	
+	// Renames Field.
+	public boolean rename(String newName) {
+		if (newName.equals(""))
+			return false;
+		this.name = newName;
+		return true;
+	}
+	
+}
