5bc81e594cb16d7d114d1d9edb7c3f7361cf5288
gps137 <39245737+gps137@users.noreply.github.com>
2022-10-19 12:08:23 -0400

    Iterator Implementations and Shift-Click Support
    Iteration implemented as a design pattern, in class IterationList. Moved ZEllipse from nested class to its own stand-alone. Updated shift-click support for Mac users who don't have a right-click option.

---
+++ src/main/java/velcro/Model/Observer.java
@@ -0,0 +1,37 @@
+/**
+ * Filename: Observer.java.
+ * 
+ * @author Jon Beare, Dylon McGrann, Greg Sinclair, Cole Stout, Benedikt Wagenlehner
+ * Course: CSCI 420 (Section 01) 
+ * Description: Observer class. Basically acts as a wrapper for ActionListener, to satisfy requirements.
+ * 
+ */
+
+
+package main.java.velcro.Model;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+
+public class Observer implements ActionListener {
+	
+	ActionListener action;
+	
+	public Observer(JComboBox input, ActionListener e) {
+		action = e;
+		input.addActionListener(action);
+	}
+	
+	public Observer(JButton input, ActionListener e) {
+		action = e;
+		input.addActionListener(action);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		action.actionPerformed(e);
+	}
+}
