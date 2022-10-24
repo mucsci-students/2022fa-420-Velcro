b83a5c04e37cabfb81379a707efed3c12ffe5f89
gps137 <39245737+gps137@users.noreply.github.com>
2022-10-24 16:28:23 -0400

    Fixes for Maven
    Some changes were ignored by Github, apparently. Updated class files.

--- src/main/java/velcro/Model/IteratorList.java
+++ src/main/java/velcro/Model/IteratorList.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Filename: IteratorList.java.
  * 
  * @author Jon Beare, Dylon McGrann, Greg Sinclair, Cole Stout, Benedikt Wagenlehner
@@ -17,21 +17,24 @@ public class IteratorList {
 
 	private List<ZEllipse> list = new ArrayList<ZEllipse>();
 	
+	// Iterator class. Iterates through a list of ZEllipse objects, per project requirement.
 	public class Iterator {
 		private IteratorList iterList;
 		private java.util.Iterator iterator;
 		private ZEllipse value;
 		
+		// Constructor.
 		public Iterator(IteratorList iteratorList) {
 			iterList = iteratorList;
 		}
 		
+		// Sets iterator to first element.
 		public void first() {
 			iterator = iterList.list.iterator();
 			next();
 		}
 		
-		
+		// Iterates iterator through list.
 		public void next() {
 			try {
 				value = (ZEllipse)iterator.next();
@@ -40,27 +43,48 @@ public class IteratorList {
 			}
 		}
 		
+		// Returns true if list is at end element.
 		public boolean isDone() {
 			return value == null;
 		}
 		
+		// Returns current list item.
 		public ZEllipse currentValue() {
 			return value;
 		}
 	}
 	
+	// Method for adding additional zellipse to list.
 	public void add(ZEllipse input) {
 		list.add(input);
 	}
 	
+	// Method for removing zellipse from list.
+	public void remove(ZEllipse input) {
+		list.remove(input);
+	}
+	
+	// Method for removing zellipse from list by name.
+	public void remove(String input) {
+		for (int i = 0; i<list.size(); i++) {
+			if (list.get(i).className.equals(input)) {
+				remove(list.get(i));
+				break;
+			}
+		}
+	}
+	
+	// Inializes and returns new iterator; multiple iterators can be moving through the list at once.
 	public Iterator getIterator() {
 		return new Iterator(this);
 	}
 	
+	// Returns size of list.
 	public int size() {
 		return list.size();
 	}
 	
+	// Clears main list.
 	public void clear() {
 		list.clear();
 		return;
--- src/main/java/velcro/Model/ZEllipse.java
+++ src/main/java/velcro/Model/ZEllipse.java
@@ -1,14 +1,14 @@
 package main.java.velcro.Model;
 
 import java.awt.geom.Ellipse2D;
+
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 
-
-import main.*;
-
 import javax.swing.JOptionPane;
+
+import main.java.velcro.main.DrawingGUI;
 
 public class ZEllipse extends Ellipse2D.Float {
 
@@ -16,6 +16,7 @@ public class ZEllipse extends Ellipse2D.Float {
 	public List<ZEllipse> destinations;
 	public Classes thisClass;
 
+	// Constructor.
 	public ZEllipse(float x, float y, float width, float height, String name, Instance thisInstance) {
 		destinations = new ArrayList<ZEllipse>();
 		setFrame(x, y, width, height);
@@ -31,32 +32,33 @@ public class ZEllipse extends Ellipse2D.Float {
 		thisClass = thisInstance.getClass(name);
 	}
 	
+	// Returns center of zellipse.
 	public Point2D center() {
 		return new Point2D.Float(this.x, this.y);
 	}
 
+	// Unused method for determining if click was inside bounds.
 	public boolean isHit(float x, float y) {
-
 		return getBounds2D().contains(x, y);
 	}
 
+	// Movement method, for use with mouse adjuster.
 	public void addX(float x) {
-
 		this.x += x;
 		if (this.x < 0) {
 			this.x = 0;
 		}
-		if (this.x >= MovingScalingEx.panelWidth - 100) {
-			this.x = MovingScalingEx.panelWidth - 100;
+		if (this.x >= DrawingGUI.panelWidth - 100) {
+			this.x = DrawingGUI.panelWidth - 100;
 		}
 		thisClass.setLocation((int) this.x, (int) this.y);
 	}
 
+	// Movement method, for use with mouse adjuster.
 	public void addY(float y) {
-
 		this.y += y;
-		if (this.y >= MovingScalingEx.panelHeight - 150) {
-			this.y = MovingScalingEx.panelHeight - 150;
+		if (this.y >= DrawingGUI.panelHeight - 150) {
+			this.y = DrawingGUI.panelHeight - 150;
 		}
 		if (this.y < 0) {
 			this.y = 0;
@@ -64,16 +66,17 @@ public class ZEllipse extends Ellipse2D.Float {
 		thisClass.setLocation((int) this.x, (int) this.y);
 	}
 
+	// Optional scaling method, to resize class circle.
 	public void addWidth(float w) {
-
 		this.width += w;
 	}
 
+	// Optional scaling method, to resize class circle.
 	public void addHeight(float h) {
-
 		this.height += h;
 	}
 
+	// Method for changing class name attribute.
 	public void setName(String input) {
 		this.className = input;
 	}
