e00931f37733ebc5ca32f2d706877e56814b1dd2
gps137 <39245737+gps137@users.noreply.github.com>
2022-10-29 10:08:44 -0400

    Update MethodsTest.java

--- src/test/java/velcro/Model/MethodsTest.java
+++ src/test/java/velcro/Model/MethodsTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import velcro.Model.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MethodsTest {
