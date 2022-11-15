59707e5c549b8ad6ba7876f4ceba265aa2f1545a
gps137 <39245737+gps137@users.noreply.github.com>
2022-11-14 21:26:36 -0500

    Attempting Classes Test
    Trying this out...

--- src/test/java/velcro/Model/ClassesTest.java
+++ src/test/java/velcro/Model/ClassesTest.java
@@ -1,26 +1,59 @@
 package main.java.velcro.Model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 import org.junit.Assert;
-import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-
+import java.awt.Point;
 import java.util.ArrayList;
-
-import static org.junit.Assert.*;
+import java.util.List;
 
 public class ClassesTest {
 
+	@Test
+	public void ClassesTest() {
+		List<Fields> fieldList = new ArrayList<Fields>();
+		Fields fieldTest = new Fields("one", "two");
+		fieldList.add(fieldTest);
+		List<Methods> methodList = new ArrayList<Methods>();
+		Methods methodTest = new Methods("three", "four", null);
+		methodList.add(methodTest);
+		List<Relationships> relationshipList = new ArrayList<Relationships>();
+		Relationships relationshipTest = new Relationships("test2","test3", "testrelation");
+		relationshipList.add(relationshipTest);
+		Classes test = new Classes("test1", fieldList, methodList, relationshipList);
+		assertEquals("test1", test.getName());
+		assertEquals(fieldList, test.fieldList);
+		assertEquals(methodList, test.methodList);
+		assertEquals(relationshipList, test.relationshipList);
+		assertEquals(new Point(0,0), test.point);
+	}
+	
+	@Test
+	public void testResize() {
+		Classes test = new Classes("test");
+		boolean check = test.resize(200, 200);
+		assertEquals(200, test.width);
+		assertEquals(200, test.height);
+		assertEquals(true, check);
+		assertEquals(false, test.resize(200, 200));
+	}
+	
+	
     @Test
     public void setLocation() {
-
         Classes test = new Classes("test");
         test.setLocation(0,0);
-
         assertEquals(0,test.point.x);
         assertEquals(0,test.point.y);
+        test.setLocation(10,10);
+        assertEquals(10,test.point.x);
+        assertEquals(10,test.point.y);
     }
 
     @Test
@@ -32,38 +65,38 @@ public class ClassesTest {
 
 
     @Test
-    public void testEquals() {/*
-        ArrayList<Fields> fieldsList = new ArrayList<>();
-        ArrayList<Methods> methodsList = new ArrayList<>();
-        ArrayList<Relationships> relationshipsList = new ArrayList<>();
-        Classes test = new Classes("name",fieldsList,methodsList,relationshipsList);
-        Classes test1 = new Classes("name",fieldsList,methodsList,relationshipsList);
-        Classes test2 = new Classes("name1",fieldsList,methodsList,relationshipsList);
-        Classes test3 = new Classes("name",fieldsList,methodsList,relationshipsList);
-        Classes test4 = new Classes("name",fieldsList,methodsList,relationshipsList);
-        assertEquals(true, test.equals(test));
-        assertEquals(true, test.equals(test1));
-        assertEquals(false, test.equals(test2));
-        test1.addRelationship("source","destination","type");
-        assertEquals(false, test.equals(test1));
-        test3.addField("source","destination");
-        assertEquals(false, test.equals(test3));
-        ArrayList<Parameters> list = new ArrayList<>();
-        list.add(new Parameters("name", "type"));
-        test3.addMethod("source","destination",list);
-        assertEquals(false, test.equals(test3));
-        */
+    public void equals() {
+        Classes test1 = new Classes("class1");
+        Classes test2 = new Classes("class2");
+        assertEquals(true, test1.equals(test1));
+        assertEquals(false, test1.equals(test2));
     }
 
     @Test
     public void rename() {
-        Classes test = new Classes("test");
+		List<Relationships> relationshipList = new ArrayList<Relationships>();
+		Relationships relationshipTest = new Relationships("test","test3", "testrelation");
+        Classes test = new Classes("test", null, null, relationshipList);
         Classes test1 = new Classes("");
-
         assertEquals(true, test.rename(test.getName(), "new"));
         assertEquals(true, test.rename("test", "new"));
+        assertEquals(true, test.rename("test3", "new2"));
+        assertEquals("new", test.relationshipList.get(0).getSource());
+        assertEquals("new2", test.relationshipList.get(0).getDestination());
         assertEquals(false, test1.rename(test.getName(), ""));
         assertEquals(false, test1.rename(test1.getName(), "new"));
+    }
+    
+    @Test
+    public void removeFieldTest() {
+    	List<Fields> fieldsList = new ArrayList<Fields>();
+    	Fields newField = new Fields("test1", "test2");
+    	Classes newClass = new Classes("test", fieldsList, null, null);
+    	assertEquals(0, fieldsList.size());
+    	fieldsList.add(newField);
+    	assertEquals(1, fieldsList.size());
+    	newClass.removeField(newField);
+    	assertEquals(0, fieldsList.size());
     }
 
     @Test
@@ -129,18 +162,20 @@ public class ClassesTest {
     public void getField() {
         Classes test = new Classes("test1");
 
+        assertEquals(null, test.getField(null));
         assertEquals(null, test.getField("test1"));
         test.addField("test1","field1");
         assertEquals(test.fieldList.get(0), test.getField("test1"));
+        assertEquals(null, test.getField("test2"));
     }
 
     @Test
     public void getMethod() {
         Classes test = new Classes("test1");
-
-        assertEquals(null, test.getField("test1"));
-        test.addField("test1","field1");
-        assertEquals(test.fieldList.get(0), test.getField("test1"));
+        assertEquals(null, test.getMethod(null));
+        assertEquals(null, test.getMethod("test1"));
+        test.addMethod("test1","field1", null);
+        assertEquals(test.methodList.get(0), test.getMethod("test1"));
     }
 
     @Test
@@ -185,5 +220,6 @@ public class ClassesTest {
 
         assertEquals(null,test.getRelationship("",""));
         assertEquals(test1.relationshipList.get(0),test1.getRelationship("source","destination"));
+        assertEquals(null,test1.getRelationship("notsource", "notdestination"));
     }
 }
