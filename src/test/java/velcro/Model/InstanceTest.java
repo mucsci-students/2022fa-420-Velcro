18694af8e5c26e736c3db37c2c076739c2b5f2e6
benninho21 <104145274+benediktwagenlehner@users.noreply.github.com>
2022-11-16 23:47:55 -0500

    Iterator list
    Added the tests. I might have to look in one method.

--- src/test/java/velcro/Model/InstanceTest.java
+++ src/test/java/velcro/Model/InstanceTest.java
@@ -15,17 +15,17 @@ public class InstanceTest {
 
     @Test
     public void setHighlight() {
-        //Instance test = new Instance();
-        //Classes testClass = new Classes("testClass");
-        //test.setHighlight(testClass);
-        //assertEquals(testClass, test.getClass("testClass"));
+        Instance test = new Instance();
+        Classes testClass = new Classes("testClass");
+        test.setHighlight(testClass);
+        assertEquals(testClass, test.highlight);
     }
 
     @Test
     public void addClass() {
-        //Instance test = new Instance();
-        //test.addClass("testClass");
-        //assertEquals("testClass", test.getClass("testClass"));
+        Instance test = new Instance();
+        test.addClass("testClass");
+        assertEquals("testClass", test.classList.get(0).getName());
     }
 
     @Test
@@ -46,19 +46,36 @@ public class InstanceTest {
     }
 
     @Test
-    public void checkClass() {
+    public void checkClass1() {
+        Instance test = new Instance();
+        assertEquals(false, test.checkClass("test"));
+        test.addClass("test");
+        assertEquals(true, test.checkClass("test"));
+        assertEquals(false, test.checkClass("testClass"));
     }
 
     @Test
     public void testCheckClass() {
+        Instance test = new Instance();
+        assertEquals(false, test.checkClass("test"));
+        test.addClass("test");
+        //assertEquals(true, test.checkClass(testClass));
+        //assertEquals(false, test.checkClass(testClass1));
     }
 
     @Test
     public void testGetClass() {
+        Instance test = new Instance();
+        assertEquals(null, test.getClass("test"));
+        test.addClass("test");
+        assertEquals("test", test.getClass("test").getName());
+        assertEquals(null, test.getClass("test1"));
     }
 
     @Test
     public void printToJson() {
+        Instance test = new Instance();
+        //JSONAssert.assertEquals(test.printToJson(), ":[]", true);
     }
 
     @Test
