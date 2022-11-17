c1790c61ada1ea5fde8b06b0d8bab288c9e8ce33
gps137 <39245737+gps137@users.noreply.github.com>
2022-11-17 18:45:03 -0500

    MovingAdapter Tests
    Added tests, rewrote a little bit

--- src/main/java/velcro/View/GUIFrame.java
+++ src/main/java/velcro/View/GUIFrame.java
@@ -154,6 +154,154 @@ public class GUIFrame extends JPanel {
 			}
 		}
 		
+		// Handles visual aspects of mouseclick.
+		public void clickHandler(MouseEvent e, MovingAdapter ma, int x, int y, boolean test) {
+			if (test)
+				return;
+			if (e.getClickCount() == 2 && e.getButton() == MouseEvent.BUTTON1) {
+				if (ma.whatItHit(GUIFrame.arr) != null) {
+					GUIFrame.thisMemento.add(GUIFrame.thisInstance);
+					ZEllipse hit = ma.whatItHit(GUIFrame.arr);
+					String newName;
+					try {
+						newName = JOptionPane.showInputDialog("Please enter a new class name!", hit.className);
+						if (newName != null) {
+							
+							try {
+								String testClass = GUIFrame.thisInstance.getClass(newName).getName();
+								JOptionPane.showInternalMessageDialog(null,
+										"Class name already in use!", "Renaming Class",
+										JOptionPane.ERROR_MESSAGE);
+							} catch (NullPointerException e3) {
+								Classes changed = GUIFrame.thisInstance.getClass(hit.className);
+								changed.rename(hit.className, newName);
+								GUIFrame.thisFrame.classModel.removeElement(hit.className);
+								hit.className = newName;
+								GUIFrame.thisFrame.classModel.addElement(newName);
+								hit.setName(newName);
+								changed = GUIFrame.thisInstance.getClass(newName);
+								GUIFrame.thisInstance.setHighlight(changed);
+							}
+						}
+					} catch (NullPointerException e2) {
+					}
+					GUIFrame.draw();
+					return;
+				}
+
+			} else if (e.getButton() == MouseEvent.BUTTON3 || e.isShiftDown() ) {
+				if (!GUIController.missAllLines(GUIFrame.lnLst, 0, (double) x, (double) y)) {
+					GUIFrame.thisMemento.add(GUIFrame.thisInstance);
+					ZLine line = GUIController.lineItHit(GUIFrame.lnLst, x, y);
+					if (JOptionPane.showConfirmDialog(null, "Delete Relationship?", "Delete Relationship",
+							JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+						GUIFrame.thisInstance.getClass(line.source).removeRelationship(line.source, line.destination);
+						GUIFrame.thisInstance.getClass(line.destination).removeRelationship(line.source, line.destination);
+						GUIFrame.lnLst.remove(line);
+						GUIFrame.draw();
+					} else {
+						return;
+					}
+
+					return;
+				}
+
+				if (ma.whatItHit(GUIFrame.arr) == null) {
+					GUIFrame.thisMemento.add(GUIFrame.thisInstance);
+					ZEllipse newEllipse = new ZEllipse(x - 40, y - 40, 80, 80, "", GUIFrame.thisInstance);
+					if (newEllipse.className != null && !newEllipse.className.equals("")) {
+						GUIFrame.arr.add(newEllipse);
+
+						GUIFrame.thisInstance.addClass(newEllipse.className);
+						GUIFrame.thisInstance.getClass(newEllipse.className).setLocation(x - 40, y - 40);
+						GUIFrame.thisFrame.classModel.addElement(newEllipse.className);
+					}
+					GUIFrame.draw();
+				} else {
+					ZEllipse hit = ma.whatItHit(GUIFrame.arr);
+					GUIFrame.thisMemento.add(GUIFrame.thisInstance);
+					GUIFrame.pntLst.add(hit);
+					if (GUIFrame.source == null) {
+						GUIFrame.source = hit.center();
+						GUIFrame.sourceName = hit.className;
+					} else {
+						Object[] types = { "Aggregation", "Composition", "Inheritance", "Realization" };
+						String n = "";
+						n = (String) JOptionPane.showInputDialog(null, "Relationship Type?", "Set Relationship Type",
+								JOptionPane.QUESTION_MESSAGE, null, types, types[0]);
+						if (n == null || n.equals("")) {
+						} else {
+							GUIFrame.thisInstance.getClass(hit.className).addRelationship(GUIFrame.sourceName, hit.className, n);
+							GUIFrame.thisInstance.getClass(GUIFrame.sourceName).addRelationship(GUIFrame.sourceName, hit.className, n);
+						}
+						GUIFrame.source = null;
+					}
+					GUIFrame.draw();
+				}
+				return;
+			} else if (e.getClickCount() == 1 && e.getButton() == MouseEvent.BUTTON1) {
+				if (ma.whatItHit(GUIFrame.arr) == null) {
+					return;
+				}
+				GUIFrame.thisMemento.add(GUIFrame.thisInstance);
+				ZEllipse hit = ma.whatItHit(GUIFrame.arr);
+				if (GUIFrame.thisInstance.getClass(hit.className) != null) {
+					GUIFrame.thisFrame.comboBox.setSelectedItem(GUIFrame.thisInstance.getClass(hit.className).getName());
+					GUIFrame.thisInstance.setHighlight(GUIFrame.thisInstance.getClass(hit.className));
+				}
+
+				Classes thisClass;
+				if (GUIFrame.thisInstance.getClass((String) GUIFrame.thisFrame.comboBox.getSelectedItem()) != null) {
+					GUIFrame.thisFrame.fieldModel.removeAllElements();
+					thisClass = GUIFrame.thisInstance.getClass((String) GUIFrame.thisFrame.comboBox.getSelectedItem());
+					for (int i = 0; i < thisClass.fieldList.size(); i++) {
+						GUIFrame.thisFrame.fieldModel.addElement(thisClass.fieldList.get(i).getName());
+					}
+				}
+
+				if (GUIFrame.thisInstance.getClass((String) GUIFrame.thisFrame.comboBox.getSelectedItem()) != null) {
+					GUIFrame.thisFrame.relationshipsModel.removeAllElements();
+					thisClass = GUIFrame.thisInstance.getClass((String) GUIFrame.thisFrame.comboBox.getSelectedItem());
+					for (int i = 0; i < thisClass.relationshipList.size(); i++) {
+
+						if (!thisClass.relationshipList.get(i).getDestination().equals(thisClass.getName())) {
+							GUIFrame.thisFrame.relationshipsModel.addElement(thisClass.relationshipList.get(i).getDestination());
+						}
+					}
+				}
+				GUIFrame.draw();
+			}
+
+		}
+		
+		// Handles visual aspect of DoMove
+		public void doMoveGui(int x, int y, int dx, int dy, boolean test) {
+			if (test)
+				return;
+			IteratorList.Iterator iter = GUIFrame.arr.getIterator();
+			for (iter.first(); !iter.isDone(); iter.next()) {
+				ZEllipse thisOne = iter.currentValue();
+				if (thisOne.isHit(x, y)) {
+					thisOne.addX(dx);
+					thisOne.addY(dy);
+					GUIFrame.draw();
+				}
+			}
+		}
+		
+		// Determine what class was clicked on.
+		public ZEllipse whatItHit(IteratorList arr, int x, int y, boolean test) {
+			if (test)
+				return null;
+			IteratorList.Iterator iter = arr.getIterator();
+			for (iter.first(); !iter.isDone(); iter.next()) {
+				if (iter.currentValue().isHit(x, y)) {
+					return iter.currentValue();
+				}
+			}
+			return null;
+		}
+
 		// Required override.
 		@Override
 		public void paintComponent(Graphics g) {
--- src/test/java/velcro/Model/MovingAdapterTest.java
+++ src/test/java/velcro/Model/MovingAdapterTest.java
@@ -1,25 +1,50 @@
 package main.java.velcro.Model;
 
-
 import org.junit.Test;
+
+import main.java.velcro.View.GUIFrame;
+import main.java.velcro.View.MenuFrame;
+import main.java.velcro.main.DrawingGUI;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import java.awt.event.MouseEvent;
+
+
 public class MovingAdapterTest {
 
-    @Test
-    public void mousePressed() {
-    }
+	@Test
+	public void movingAdapterTest() {
+		
+		Instance thisInstance = new Instance();
+		DrawingGUI ex = new DrawingGUI(thisInstance);
+		GUIFrame newFrame = new GUIFrame(thisInstance);
+//		ex.resize();
+//		MenuFrame menuFrame = new MenuFrame(thisInstance, ex, 1000);
+//		ex.setFrame(menuFrame);
+//		menuFrame.firstLoad();
+//		
+		MouseEvent testClick = new MouseEvent(newFrame, 0, 0, 0, 0, 0, 0, 0, 1, false, 1);
+		MouseEvent testClick2 = new MouseEvent(newFrame, 0, 0, 0, 100, 100, 0, 0, 1, false, 1);
+		MouseEvent testClick3 = new MouseEvent(newFrame, 0, 0, 0, 100, 100, 0, 0, 1, false, 3);
+		MouseEvent testClick4 = new MouseEvent(newFrame, 0, 0, 0, 0, 0, 0, 0, 1, false, 3);
+		MouseEvent testClick5 = new MouseEvent(newFrame, 0, 0, 0, 100, 100, 0, 0, 2, false, 3);
+		MouseEvent testClick6 = new MouseEvent(newFrame, 0, 0, 0, 0, 0, 0, 0, 2, false, 3);
 
-    @Test
-    public void mouseDragged() {
-    }
+		
+		MovingAdapter test1 = new MovingAdapter();
+		test1.test = true;
+		
+		
+		test1.mousePressed(testClick);
+		test1.mouseDragged(testClick);
+		test1.mouseClicked(testClick);
+		test1.doMove(testClick);
+		test1.whatItHit(null);
+		
+	}
 
-    @Test
-    public void mouseClicked() {
-    }
-
-    @Test
-    public void whatItHit() {
-    }
 }
